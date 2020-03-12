0735971d0d5ec5b17171d637a6ab3dd46417287b
anekom <andreeiitaa100@gmail.com>
2020-03-12 22:07:37 +0100

    getters y setters

--- Practica1_Almacen/src/empresa_almacen/Pedidos.java
+++ Practica1_Almacen/src/empresa_almacen/Pedidos.java
@@ -27,4 +27,58 @@ public class Pedidos {
 		this.estimated_date = estimated_date;
 	}
 	
+	
+	public ArrayList<Producto> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Producto> products) {
+		this.products = products;
+	}
+	
+	
+	public ArrayList<Integer> getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(ArrayList<Integer> quantity) {
+		this.quantity = quantity;
+	}
+	
+
+	public Direccion getDelivery_address() {
+		return delivery_address;
+	}
+
+	public void setDelivery_address(Direccion delivery_address) {
+		this.delivery_address = delivery_address;
+	}
+
+	
+	public Clientes getClient() {
+		return client;
+	}
+
+	public void setClient(Clientes client) {
+		this.client = client;
+	}
+
+
+	public Date getEstimated_date() {
+		return estimated_date;
+	}
+
+	public void setEstimated_date(Date estimated_date) {
+		this.estimated_date = estimated_date;
+	}
+
+	
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+	
 }
