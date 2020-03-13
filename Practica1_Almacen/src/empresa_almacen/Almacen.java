fde2e598c3ed3b9593e1f341c6276f270fe52f3b
IMoll <nachomollamoros@gmail.com>
2020-03-13 11:12:08 +0100

    Actualización de XML en la clase Almacen

--- Practica1_Almacen/src/empresa_almacen/Almacen.java
+++ Practica1_Almacen/src/empresa_almacen/Almacen.java
@@ -1,52 +1,20 @@
 package empresa_almacen;
 
 import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlAttribute;  
+import javax.xml.bind.annotation.XmlElement;  
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class Almacen {
 	
-
-
 	private ArrayList<Clientes> clientes = new ArrayList<Clientes>();
 	private ArrayList<Producto> products = new ArrayList<Producto>();
 	private ArrayList<Pedidos> pedidos = new ArrayList<Pedidos>();
 		
-	
-	public ArrayList<Clientes> getClientes() {
-		return clientes;
-	}
-
-
-	public void setClientes(ArrayList<Clientes> clientes) {
-		this.clientes = clientes;
-	}
-
-
-	public ArrayList<Producto> getProducts() {
-		return products;
-	}
-
-
-	public void setProducts(ArrayList<Producto> products) {
-		this.products = products;
-	}
-
-
-	public ArrayList<Pedidos> getPedidos() {
-		return pedidos;
-	}
-
-
-	public void setPedidos(ArrayList<Pedidos> pedidos) {
-		
-		this.pedidos = pedidos;
-	}
-
-
-		
 	public Almacen() {
 		super();
 	}
-
 
 	public Almacen(ArrayList<Clientes> clientes, ArrayList<Producto> products, ArrayList<Pedidos> pedidos) {
 		super();
@@ -54,9 +22,4 @@ public class Almacen {
 		this.products = products;
 		this.pedidos = pedidos;
 	}
-	
-	
-	
-	
-
 }
